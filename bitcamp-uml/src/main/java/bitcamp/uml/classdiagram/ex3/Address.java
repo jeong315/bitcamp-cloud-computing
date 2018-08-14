c75e58e31550e649b8aa5bdda805b8da427f3e81
jeong315 <ghlwlscjswo@gmail.com>
2018-08-14 17:57:27 +0900

    .

---
+++ bitcamp-uml/src/main/java/bitcamp/uml/classdiagram/ex3/Address.java
@@ -0,0 +1,14 @@
+package bitcamp.uml.classdiagram.ex3;
+
+public class Address {
+
+    String postNo;
+    String basicAddr;
+    String detailAddr;
+    
+    public Address(String postNo, String basicAddr, String detailAddr) {
+        this.postNo = postNo;
+        this.basicAddr = basicAddr;
+        this.detailAddr = detailAddr;
+    }
+}
